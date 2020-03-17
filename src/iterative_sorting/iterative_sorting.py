bf21ac43006a949dd43aead5f9a2226c82a3aa5c
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-03-17 14:24:05 -0600

    guided project insertion added

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,29 @@
+#Insertion sort guided project
+
+my_list = [1, 2, 10, 15, 66, 7, 9, 45, 16]
+
+def insertion_sort(list_to_sort):
+    #separate the first element, think of it as sorted
+
+    #for all other indices, starting at 1
+    for i in range(1, len(list_to_sort)):
+    #put current number in a temp variable
+        temp = list_to_sort[i]
+    #look left, until with find where it belongs
+        j = i
+        while j > 0 and temp < list_to_sort[j-1]:
+    #as we look left shift items to the right as we iterate
+            list_to_sort[j] = list_to_sort[j-1]
+            j-= 1
+    #When left is smaller than temp, or we are at zero, put at this spot.
+        list_to_sort[j] = temp
+
+    return list_to_sort
+
+
+print(insertion_sort(my_list))
+
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
